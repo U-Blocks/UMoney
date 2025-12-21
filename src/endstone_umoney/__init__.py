1da055bed7cf3f1c1ad1b28a2da685775fb07eaa
umarurize <3229187183@qq.com>
2025-12-21 21:41:00 +0800

    refactor

--- src/endstone_umoney/__init__.py
+++ src/endstone_umoney/__init__.py
@@ -1,4 +1,7 @@
 from endstone_umoney.umoney import umoney
-from endstone_umoney.lang import lang
+from endstone_umoney.lang import load_lang_data
 
-__all__ = ['umoney', 'lang']
+__all__ = [
+    "umoney",
+    "load_lang_data"
+]
